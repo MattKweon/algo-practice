3b08b2d2e2f4571f983dbec15905cf23b84cde5b
Matthew Kweon <kweon.matt@gmail.com>
2023-01-28 00:37:38 +0000

    Longest Common Prefix

--- main.js
+++ main.js
@@ -51,10 +51,13 @@ function romanToInt(s) {
 // Write a function to find the longest common prefix string amongst
 // an array of strings.
 // If there is no common prefix, return an empty string "".
-// function longestCommonPrefix(strs) {
-//   output = '';
+function longestCommonPrefix(strs) {
+  if (!strs.length) return '';
 
-//   for (let i = 0; i < strs.length; i++) {
-
-//   }
-// }
+  for (let i = 0; i <= strs[0].length; i++) {
+    if (!strs.every(string => string[i] === strs[0][i])) {
+      return strs[0].slice(0, i);
+    }
+  }
+  return strs[0];
+}
