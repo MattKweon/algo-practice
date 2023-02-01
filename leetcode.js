96c9667c9b61e627a8b7015dd3bb88142b7ee893
Matthew Kweon <kweon.matt@gmail.com>
2023-02-01 04:06:49 +0000

    Best Team With No Conflicts

--- leetcode.js
+++ leetcode.js
@@ -202,3 +202,31 @@ function plusOne(digits) {
   const output = (BigInt(digits.join('')) + BigInt(1)).toString().split('');
   return output;
 }
+
+/*
+// Best Team With No Conflicts
+//
+// pseudocode:
+[] create an array that contains the age/score pair of a player
+[]
+*/
+function bestTeamScore(scores, ages) {
+  const n = ages.length;
+  const ageScorePair = new Array(n);
+  for (let i = 0; i < n; i++) {
+    ageScorePair[i] = [ages[i], scores[i]];
+  }
+  ageScorePair.sort((a, b) => a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]);
+  const dp = new Array(n).fill(0);
+  let maxScore = 0;
+  for (let i = 0; i < n; i++) {
+    dp[i] = ageScorePair[i][1];
+    for (let j = 0; j < i; j++) {
+      if (ageScorePair[i][1] >= ageScorePair[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + ageScorePair[i][1]);
+      }
+    }
+    maxScore = Math.max(maxScore, dp[i]);
+  }
+  return maxScore;
+}
