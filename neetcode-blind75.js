5ff5ca7dbbe1b3f4d944d90435ff2bd321261964
Matthew Kweon <kweon.matt@gmail.com>
2023-02-02 02:52:44 +0000

    Completed Valid Anagram

--- neetcode-blind75.js
+++ neetcode-blind75.js
@@ -14,3 +14,22 @@ function containsDuplicate(nums) {
   const s = new Set(nums);
   return s.size !== nums.length;
 }
+
+/*
+// 242. Valid Anagram
+//
+// pseudocode:
+[] split up both of the input strings into an array of individual characters
+[] sort the new arrays by alphabetical order
+[] if the length of of the two strings do not equal each other return false
+[] loop through the first string and make sure all the characters match
+*/
+function isAnagram(s, t) {
+  s = s.split('').sort();
+  t = t.split('').sort();
+  if (s.length !== t.length) return false;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== t[i]) return false;
+  }
+  return true;
+}
