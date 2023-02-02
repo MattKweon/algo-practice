e5d6c7e0477272b59b395c27c3d316874dd0d95d
Matthew Kweon <kweon.matt@gmail.com>
2023-02-02 02:14:33 +0000

    Completed Contains Duplicate

---
+++ neetcode-blind75.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+//
+// Blind 75 - neetcode
+//
+
+/*
+// 217. Contains Duplicate
+//
+// pseudocode:
+[x] create a new set and compare the size of the set to the length of the input integer array
+   to see if they equal each other
+*/
+function containsDuplicate(nums) {
+  const s = new Set(nums);
+  return s.size !== nums.length;
+}
